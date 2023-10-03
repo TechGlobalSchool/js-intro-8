06f4b3a87c0811864dfd44503d953793593e077b
TechGlobal <tgschool01@gmail.com>
2023-10-02 19:51:56 -0700

    added number properties and functions

--- js-basics/19-numbers.js
+++ js-basics/19-numbers.js
@@ -23,3 +23,35 @@ console.log('The result is = ' + c - b); // NaN - Not a Number
 // NaN - Not a Number
 console.log(NaN); // NaN
 console.log(typeof NaN); // number
+
+console.log("" + 3); // '3'
+console.log(null + 5); // 5
+console.log(false + 5); // 5
+
+console.log(0 / 0); // NaN
+console.log(0 / 5); // 0
+console.log(0 / 10); // 0
+
+console.log(10 / 0); // Infinity
+
+// INFINITY VS -INFINITY
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(typeof Infinity); // number
+console.log(typeof -Infinity); // number
+
+console.log(Infinity + Infinity); // Infinity
+console.log(Infinity - Infinity); // NaN
+console.log(Infinity / Infinity); // NaN
+console.log(Infinity * Infinity); // Infinity
+console.log(Infinity % Infinity); // NaN
+console.log(Infinity ** Infinity); // Infinity
+
+// isNaN() function - this is going to help us check if a variable is number or not
+var v1 = 'Hello', v2 = '25', v3 = 3, v4 = 5.5;
+
+console.log(isNaN(v1)); // true
+console.log(isNaN(v2)); // false
+console.log(isNaN(v3)); // false
+console.log(isNaN(v4)); // false
