a2783e4147f5d0a583ab962d37fa70aeac44f5d2
TechGlobal <tgschool01@gmail.com>
2023-10-04 20:59:50 -0700

    added string methods

--- js-basics/22-string-functions.js
+++ js-basics/22-string-functions.js
@@ -31,4 +31,3 @@ let quantity = 5, product = 'Pen', price = 2;
 // I bought 5 Pen items for $10.
 console.log('I bought ' + quantity + ' ' + product + ' items for $' + quantity * price + '.');
 console.log(`I bought ${quantity} ${product} items for $${quantity * price}.`);
-
