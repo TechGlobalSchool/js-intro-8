7a29ab6444a42fc580d2042967cb3889ca7ac6e4
akinkayagithub <akinkaya.b@gmail.com>
2023-10-14 11:45:43 -0500

    added if else intro

---
+++ js-basics/35-conditional-statements.js
@@ -0,0 +1,28 @@
+// Teenage
+
+let age = 5;
+
+if(age >= 10 && age <= 19) { // this block will execute only if the condition is true
+    console.log(`${age} is a teenage!`);
+}
+else { // this block will execute only if the condition is false
+    console.log(`${age} is NOT a teenage!!!`);
+}
+
+console.log('End of the program!');
+
+
+// Another example of if else 5
+
+let number1 = 3, number2 = 1;
+
+if(number1 > number2) { // number1 is greater
+    console.log('This is if block running');
+    console.log(`The absolute difference = ${number1 - number2}`);
+}
+else { // number 1 is not greater
+    console.log('This is else block running');
+    console.log(`The absolute difference = ${number2 - number1}`);
+}
+
+console.log('End of the program!');
