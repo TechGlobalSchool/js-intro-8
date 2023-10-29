39ba7aac73e53d6634e1616c2b49f3280b199abe
akinkayagithub <akinkaya.b@gmail.com>
2023-10-29 14:06:29 -0500

    added slice and splice methods

--- js-arrays/Recap.js
+++ js-arrays/Recap.js
@@ -1,24 +1,9 @@
+let str = 'TechGlobal';
 
-let str = 'first of first';
+console.log(str.slice(0, 4)); // 'Tech'
+console.log(str.slice(4)); // 'Global'
 
-str.includes('f'); // true
-str.indexOf('f'); // 0
-str.lastIndexOf('f'); // 9
+console.log(str.slice(-3)); // 'bal'
 
-str.includes('first'); // true
-str.indexOf('first'); // 0
-str.lastIndexOf('first'); // 9 
-
-str.includes('abc'); // false
-str.indexOf('abc'); // -1
-str.lastIndexOf('abc'); // -1 
-
-
-
-let s1 = 'Tech', s2 = 'Global';
-
-console.log(s1 + s2);
-console.log(s1.concat(s2));
-console.log(`${s1}${s2}`);
-
-console.log(s1.split(''));
+console.log(str.slice(-2, -4)); // ''
+console.log(str.slice(-4, -2)); // 'ob'
