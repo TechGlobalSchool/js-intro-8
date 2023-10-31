fbd5684cd640621aef3525a4431c5b2ec6da0be8
akinkayagithub <akinkaya.b@gmail.com>
2023-10-30 20:54:11 -0500

    added array methods and destructuring

--- js-arrays/Recap.js
+++
@@ -1,9 +0,0 @@
-let str = 'TechGlobal';
-
-console.log(str.slice(0, 4)); // 'Tech'
-console.log(str.slice(4)); // 'Global'
-
-console.log(str.slice(-3)); // 'bal'
-
-console.log(str.slice(-2, -4)); // ''
-console.log(str.slice(-4, -2)); // 'ob'
