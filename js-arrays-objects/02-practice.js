3ab2ed772e72dbfd81351b953f489fa5f4cbcc33
akinkayagithub <akinkaya.b@gmail.com>
2023-11-07 21:01:40 -0600

    added practices 2

--- js-arrays-objects/02-practice.js
+++ js-arrays-objects/02-practice.js
@@ -56,3 +56,23 @@ const users = [
 console.log(users.length); // 4
 
 // Count how many users are older than 30   -> 2
+// Count how many users live in Chicago     -> 2
+// Find and store all the users name younger than 35    -> [ 'Tech', 'Jane', 'Alex' ]
+// Find average of ages of all users    -> 28
+
+let count30Plus = 0;
+let countChicago = 0;
+const younger35 = [];
+let sumAge = 0;
+
+for(const user of users) {
+    if(user.age > 30) count30Plus++;
+    if(user.address.city.toLowerCase() === 'chicago') countChicago++;
+    if(user.age < 35) younger35.push(user.firstName);
+    sumAge += user.age;
+}
+
+console.log(count30Plus); // 2
+console.log(countChicago); // 2
+console.log(younger35); // [ 'Tech', 'Jane', 'Alex' ]
+console.log(sumAge / users.length); // 28
