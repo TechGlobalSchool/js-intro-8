e5c11da08687e5ad43f17e0dbc1ce1002382fdd9
akinkayagithub <akinkaya.b@gmail.com>
2023-11-21 23:20:03 -0600

    added es6 classes

--- js-oop/02-js-constructor.js
+++ js-oop/02-js-constructor.js
@@ -20,7 +20,7 @@ car3 -> 2020 Tesla Y 27000
 3. Get each cars price information in the format -> {year make model} is ${price}.
 Example: 2023 Tesla X is $80000.
 */
-
+// BEFORE ES6
 function Car(year, make, model, price) {
     this.year = year;
     this.make = make;
@@ -29,11 +29,11 @@ function Car(year, make, model, price) {
 }
 
 Car.prototype.drive = function() {
-    console.log(`${this.year, this.make, this.model} drives.`);
+    console.log(`${this.year} ${this.make} ${this.model} drives.`);
 };
 
 Car.prototype.stop = function() {
-    console.log(`${this.year, this.make, this.model} stops.`);
+    console.log(`${this.year} ${this.make} ${this.model} stops.`);
 };
 
 const car1 = new Car(2023, 'Tesla', 'X', 80000);
@@ -42,8 +42,22 @@ const car3 = new Car(2020, 'Tesla', 'Y', 27000);
 
 const allcars = [car1, car2, car3];
 
-allcars.forEach(car => console.log(car));
+// Using forEach() method
+allcars.forEach(car => {
+    console.log(car);
+    car.drive();
+    car.stop();
+    console.log(`${car.year} ${car.make} ${car.model} is $${car.price}.`);
+    console.log('-------------------------------------------------------');
+});
 
+
+
+// Using for...of loop
 for(const car of allcars) {
     console.log(car);
+    car.drive();
+    car.stop();
+    console.log(`${car.year} ${car.make} ${car.model} is $${car.price}.`);
+    console.log('-------------------------------------------------------');
 }
