950fd27d5f9af940e793f3d59cfb0e0f313f4955
akinkayagithub <akinkaya.b@gmail.com>
2023-11-22 20:54:27 -0600

    added inheritance

---
+++ js-oop/09-js-es6-inheritance.js
@@ -0,0 +1,30 @@
+/*
+Create a class called as Animal
+Create a constructor which takes 'name', 'age', 'color'
+Create 2 functions eat(), run()
+*/
+
+
+
+/*
+Create a class called as Cat and inherit all the Animal class properties and methods
+Create a constructor which takes 'name', 'age', 'color', 'breed'
+Create 1 function play()
+*/
+
+
+/*
+Create a class called as Dog and inherit all the Animal class properties and methods
+Create a constructor which takes 'name', 'age', 'color', 'breed'
+Create 1 function catch()
+*/
+
+
+/*
+Create a class called as Husky and inherit all the Dog class properties and methods
+Create a constructor which takes 'name', 'age', 'color', 'breed', 'shed'
+Create 1 function protect()
+*/
+
+
+// TRY THIS WITH PROTOTYPAL INHERITANCE AS WELL
