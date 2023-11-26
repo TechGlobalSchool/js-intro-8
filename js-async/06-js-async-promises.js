1f2ccdbf8002f75585e228ec65b3ce973995d4d6
TechGlobalSchool <tgschool01@gmail.com>
2023-11-26 13:21:15 -0800

    added promises

---
+++ js-async/06-js-async-promises.js
@@ -0,0 +1,49 @@
+// Syntax of Promise 
+const myPromise = new Promise((resolve, reject) => {
+    resolve('Fullfilled! | Successful!');
+    reject('ERROR! | FAILED');
+});
+
+
+console.log(myPromise); // Promise { 'Fullfilled! | Successful!' }
+
+
+const promiseAge = new Promise((resolve, reject) => {
+    const age = Math.floor(Math.random() * 30);
+    console.log(`Age is = ${age}.`);
+    if (age >= 16) {
+        resolve();
+    }
+    else reject();
+});
+
+
+promiseAge.then(() => {
+    // Success body
+    console.log('THE AGE IS ALLOWED!');
+}).catch(() => {
+    // Fail body
+    console.log('ERROR! THE AGE IS NOT ALLOWED!');
+});
+
+
+// Returning promises in a function
+function testAge() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const age = Math.floor(Math.random() * 30);
+            console.log(`Age is = ${age}.`);
+            if(age >= 16) {
+                resolve();
+            }
+            else reject();
+        }, 2000);
+    });
+}
+
+testAge().then(() => {
+    // Success code
+    console.log('YOU CAN GET DL WITH AN EXAM!');
+}).catch(() => {
+    console.log('ERROR: YOU CANNOT GET DL!');
+});
