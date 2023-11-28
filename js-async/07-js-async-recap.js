0a64a958c59f0d21863ab558746eb41fa1c4753f
akinkayagithub <akinkaya.b@gmail.com>
2023-11-27 20:06:30 -0600

    added async await

--- js-async/07-js-async-recap.js
+++ js-async/07-js-async-recap.js
@@ -30,10 +30,11 @@ Validation is FAILED!
 */
 
 // Fixing the issue with callback
-function goToURL(callback) {
+function goToURL(callback, error) {
     setTimeout(() => {
         console.log('You are on TechGlobal Website');
-        callback();
+        if(Math.floor(Math.random() * 2)) callback();
+        else error();
     }, 5000);
 }
 
@@ -48,6 +49,8 @@ function validateMainHeading() {
 function test1() {
     goToURL(() => {
         validateMainHeading();
+    }, () => {
+        console.log('ERROR');
     });  
 }
 
@@ -85,5 +88,37 @@ test1();
 
 
 
-if(Math.floor(Math.random() * 2)) console.log('SUCCESS');
-else console.log('FAILURE');
+// Fixing the issue with Async - Await keywords
+function goToURL() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(Math.floor(Math.random() * 2)) resolve('URL is loaded!');
+            else reject('Website is broken!');
+        }, 2000);
+    });
+}
+
+function validateMainHeading() {
+    const actualText = 'Looking to pursue a new career?'; // You will learn how to get this later
+    const expectedText = 'Looking to pursue a new career?'; // Given in the requirements
+    if(expectedText === actualText) console.log('Validation is SUCCESSFUL!');
+    else console.log('Validation is FAILED!');
+}
+
+
+async function test1() {
+    // goToURL().then(() => {
+    //     validateMainHeading();
+    // }).catch((err) => {
+    //     console.log(err);
+    // }); 
+
+    try {
+        await goToURL();
+        validateMainHeading();
+    } catch(err) {
+        console.log('ERROR:', err);
+    }
+}
+
+test1();
